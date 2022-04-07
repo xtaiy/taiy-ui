506c20050741309548b97f8d57e77d8ddf9d7320
taiy <1726311422@qq.com>
2022-04-07 21:11:56 +0800

    完善aside切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@ import { ref } from "@vue/reactivity"
 import { provide } from "@vue/runtime-core"
 
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  const menuVisible=ref(false)
+  const width=document.documentElement.clientWidth
+  const menuVisible=ref(width>700?true:false)
   provide('menuVisible',menuVisible)
  
 
