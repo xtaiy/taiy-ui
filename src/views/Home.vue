51a85688de27da2d31fc4f854ae501295ac3e8df
taiy <1726311422@qq.com>
2022-04-07 19:56:54 +0800

    Home页面CSS更改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
         <div class="banner">
             <h1>taiy的ui</h1>
             <h2>一个不错的额UI框架</h2>
-            <p>
+            <p class="action">
                 <a href="">Github</a>
                 <a href="">开始</a>
             </p>
@@ -20,5 +20,27 @@ components:{
 </script>
 
 <style lang="scss" scoped>
+    .banner{
+        background: lightgreen;
+        padding: 100px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        >.action{
+            padding: 8px 0;
+            a{
+                background: #fff;
+                margin: 0 8px;
+                display: inline-block;
+                $h:28px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 8px;
+            }
+            
 
+        }
+    }
 </style>
