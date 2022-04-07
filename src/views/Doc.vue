9e6c11eb28f4d15f5020b89c6a7a2164e0184ee9
taiy <1726311422@qq.com>
2022-04-07 19:10:15 +0800

    搭建html结构

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        Doc 页面
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="content">
+            <aside>边栏</aside>
+            <main>主内容</main>
+        </div>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        Home 页面
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>taiy的ui</h1>
+            <h2>一个不错的额UI框架</h2>
+            <p>
+                <a href="">Github</a>
+                <a href="">开始</a>
+            </p>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.topnav{border: 1px solid red;}
+</style>
