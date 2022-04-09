33a7ea7ea755b60ba2d0485477c324c8d53e3e13
taiy <1726311422@qq.com>
2022-04-09 14:10:19 +0800

    基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,27 @@
 <template>
+    <div>Button 实例</div>
+    <h1>示例1</h1>
     <div>
-        <p>Button 的文档</p>
-        <p>Button 的文档</p>
-        <p>Button 的文档</p>
-        
+        <Button>按钮</Button>
+        <Button theme="button">按钮</Button>
+        <Button theme="link">链接</Button>
+        <Button theme="text">文本</Button>
     </div>
+    
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+
+export default{
+    components:{
+        Button
+    },
+    setup(){
+        const onClick=()=>{
+            
+        }
+        return {onClick}
+    }
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,61 @@
+<template>
+        <button class="taiy-button" 
+        :class="`theme-${theme}`" >
+        <!--$attrs有父组件传递来的属性，默认会继承到根节点上，使用v-bind改定到现在的节点上-->
+            <slot />
+        </button>
+</template>
+
+<script lang="ts">
+    export default{
+        //inheritAttrs:false,
+        /*取消默认的继承属性，这样外面的属性不会
+        自动的放在组件的最外层的标签上 */
+        props:{
+            theme:{
+                type:String,
+                default:'button'
+            }
+        },
+        setup(props,context){
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $h:32px;
+    $borfer-color:#d9d9d9;
+    $color:#333;
+    $blue:#40a9ff;
+    $radius:4px;
+    .taiy-button{
+        box-sizing: border-box;
+        height: $h;
+        padding: 0 12px;
+        cursor: pointer;
+        display: inline-block;
+        justify-content: center;
+        align-items: center;
+        white-space: nowrap;
+        background: white;
+        color: $color;
+        border: 1px solid $borfer-color;
+        box-shadow: 0 1px 0 fade-out(black, 0.95);
+        & + &{
+            margin-left: 8px;
+        }
+        &:hover,
+        &:focus{
+            color: $blue;
+            border-color: $blue;
+        }
+        &:focus{
+            outline: none;
+        }
+        &::-moz-focus-inner{
+            border: 0;
+        }
+
+    }
+</style>
