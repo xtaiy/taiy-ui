121f69b759f4c17543ef3eae9d798d869a269b64
taiy <1726311422@qq.com>
2022-04-09 09:45:35 +0800

    点击button可以切换了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,16 @@
 <template >
-    <button><span></span></button>
+    <button :class="{checked: checked}" @click="toggle"><span></span></button>
 </template>
+
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+
+let checked = ref(false)
+const toggle=()=>{
+    checked.value=!checked.value
+}
+
+</script>
 
 <style lang="scss" scoped>
     $h:22px;
@@ -9,8 +19,8 @@
         height: $h;
         width: $h * 2;
         border: none;
-        background: blue;
-        border-radius: $h/2;
+        background: grey;
+        border-radius: $h / 2;
         position: relative;
     }
     span{
@@ -20,6 +30,15 @@
         width: $h2;
         top: 2px;
         left: 2px;
-        border-radius: $h2 / 2;
+        border-radius: $h2/2;
     }
+    button.checked{
+        background: blue;
+        >span{
+            left:24px;
+        }
+    }
+    
+    
+
 </style>
